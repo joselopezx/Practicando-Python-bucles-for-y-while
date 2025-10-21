bda24205b8ec762d3df2936742d0afc23383372f
joselopezx <jose0xlopez@gmail.com>
2025-10-20 20:51:13 -0600

    Add files via upload

---
+++ Cuenta Regresiva.py
@@ -0,0 +1,23 @@
+"""
+Aline está implementando una funcionalidad que muestra mensajes 
+personalizados para los clientes durante una promoción especial 
+de su nueva librería. El sistema debe mostrar un mensaje de cuenta 
+regresiva personalizado para cada número de 10 a 1, y al final mostrar 
+el mensaje: "¡Aprovecha la promoción ahora!".
+
+Crea un programa que utilice un bucle for para mostrar los siguientes mensajes:
+
+Para números pares, muestra: "Faltan solo <número> segundos - ¡No pierdas esta oportunidad!".
+Para números impares, muestra: "La cuenta continúa: <número> segundos restantes.".
+Al final de la cuenta, muestra el mensaje: "¡Aprovecha la promoción ahora!".
+"""
+
+numero = 10
+
+while numero > 0:
+    if (numero%2)==1:
+        print(f"La cuenta contunúa : {numero} segundos restantes")
+    else:
+        print(f"Faltan solo {numero}, segundos restantes")
+    numero-=1
+print("¡Aprovecha la promoción ahora!")
